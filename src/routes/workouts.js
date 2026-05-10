d24c73707e737ee83873cccde789d7d587fae77a
victoriaaa111 <victoria.mutruc@gmail.com>
2026-05-10 19:30:32 +0300

    add POST /workouts with nested exercise creation

--- src/routes/workouts.js
+++ src/routes/workouts.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { authenticateJWT } = require('../middleware/authenticate');
-const { listWorkouts } = require('../controllers/workoutsController');
+const { listWorkouts, createWorkout } = require('../controllers/workoutsController');
 
 const router = express.Router();
 
 router.use(authenticateJWT);
 
 router.get('/', listWorkouts);
+router.post('/', createWorkout);
 
 module.exports = router;
