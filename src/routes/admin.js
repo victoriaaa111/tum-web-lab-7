7e19d26fe124ab1132337821505faa2a53bd3750
victoriaaa111 <victoria.mutruc@gmail.com>
2026-05-11 21:29:08 +0300

    add GET, PATCH, DELETE /admin/users/:id

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { authenticateJWT, requireRole } = require('../middleware/authenticate');
-const { listUsers } = require('../controllers/adminController');
+const { listUsers, updateUser, deleteUser, listUserWorkouts, listUserSessions } = require('../controllers/adminController');
 
 const router = express.Router();
 
 router.use(authenticateJWT, requireRole('ADMIN'));
 
 router.get('/users', listUsers);
+router.patch('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+router.get('/users/:id/workouts', listUserWorkouts);
+router.get('/users/:id/sessions', listUserSessions);
 
 module.exports = router;
